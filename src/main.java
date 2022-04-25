35d6e5afb517fdb190e529a12bda19deba841385
tevuori <kuba.horak00754@gmail.com>
2022-04-25 22:35:15 +0200

    Working function

--- src/main.java
+++ src/main.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
-        LocalDateTime timeanddate = LocalDateTime.now();
         System.out.println("When will the alarm hit? (Hours:Minutes)");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
 
         String[] inputsplitted = input.split(":");
-        String hours = inputsplitted[0];
-        String minutes = inputsplitted[1];
-
+        int hours = Integer.parseInt(inputsplitted[0]);
+        int minutes = Integer.parseInt(inputsplitted[1]);
+        Boolean alarm = false;
+        while (alarm==false){
+            LocalDateTime timeanddate = LocalDateTime.now();
+            if(timeanddate.getHour() == hours && timeanddate.getMinute() == minutes){
+                alarm = true;
+            }
+        }
+        if(alarm = true){
+            System.out.println("RINGING!");
+        }
     }
 }
