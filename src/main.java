3aea870b2a187a5570e5ca7a8a2c9c03aa6e39a9
tevuori <kuba.horak00754@gmail.com>
2022-04-25 22:28:21 +0200

    Main function ..

--- src/main.java
+++ src/main.java
@@ -1,5 +1,17 @@
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args){
-        
+        LocalDateTime timeanddate = LocalDateTime.now();
+        System.out.println("When will the alarm hit? (Hours:Minutes)");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        String[] inputsplitted = input.split(":");
+        String hours = inputsplitted[0];
+        String minutes = inputsplitted[1];
+
     }
 }
